dc673e0859698e33d5aa562b865b204df28bbf27
parhams <samani.m@gmail.com>
2025-11-04 12:21:45 +0330

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Hello extends React.Component {
+  render() {
+    return (<h1>!سلام دوستان من</h1>)
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Timer extends React.Component {
+  render() {
+    return (<h2>it is {new Date().toLocaleTimeString()}</h2>)
+  }
+}
+
+class App extends React.Component {
+  render(){
+  return (<div>
+    <Hello/>
+    <Timer/>
+    </div>)
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+const tick = ()=> {
+ root.render(<App/>)
+}
+
+ setInterval(()=>{
+   tick();
+ }, 1000)
